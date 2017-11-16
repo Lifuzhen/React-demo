9ead05e224dc4228c89d56c369b77448e25dcf5c
toutyio <lifuzhen@touty.io>
2017-11-16 18:48:38 +0800

    ant style

--- src/entries/index.js
+++ src/entries/index.js
@@ -4,6 +4,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import "antd/dist/antd.css";
 import Home from "./home";
 
 ReactDOM.render(<Home />, document.getElementById('root'));
